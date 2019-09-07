7d0f5a191e890db8a6f2b87db489d988e7d59233
vicente.ruan <vicente.ruan@mo-co.com>
2019-09-07 20:00:18 +0800

    初始化基础版本
    Change-Id: I796158869d7a72a065182ecf0db05433cda8ae6b
    Signed-off-by: vicente.ruan <vicente.ruan@mo-co.com>

---
+++ product-application-runner/src/main/java/org/becloud/dms/product/app/BootApplication.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.becloud.dms.product.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author vicente.ruan
+ *
+ */
+@SpringBootApplication
+public class BootApplication {
+
+}
